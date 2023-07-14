0237e0bf72d212248b9cd13c54a4a76285a77b82
Bug <77031032+0zBug@users.noreply.github.com>
2023-07-14 18:45:15 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -11,10 +11,37 @@ return function(Table)
 				rawset(Table, Character, rawget(Table, Character) or {Value})
 				Table = rawget(Table, Character)
 			end
+			
+			local Children = rawget(Table, 2)
+				
+			if Children then
+				rawset(Children, #Children + 1, Value)
+			else
+				rawset(Table, 2, {Value})
+			end
 		end
 	end
 	
 	return setmetatable(Search, {
+		__call = function(self, Index)
+			local Characters = string.split(string.lower(Index), "")
+			
+			if #Characters == 1 then
+				return rawget(rawget(Search, Characters[1]), 2)
+			end
+			
+			local Value = Search
+			
+			for _, Character in next, Characters do
+				if rawget(Value, Character) then
+					Value = rawget(Value, Character)
+				else
+					return nil
+				end
+			end
+			
+			return Value[2]
+		end,
 		__index = function(self, Index)
 			local Characters = string.split(string.lower(Index), "")
 			
@@ -41,6 +68,14 @@ return function(Table)
 			for _, Character in next, Characters do
 				rawset(Table, Character, rawget(Table, Character) or {Value})
 				Table = rawget(Table, Character)
+				
+				local Children = rawget(Table, 2)
+				
+				if Children then
+					rawset(Children, #Children + 1, Value)
+				else
+					rawset(Table, 2, {Value})
+				end
 			end
 		end
 	})
