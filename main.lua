ca82bf88b12bd5dec5dbf82228b16f54b12a701c
Bug <77031032+0zBug@users.noreply.github.com>
2023-07-14 19:00:46 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 
 return function(Table)
 	local Search = {}
-	
+
 	if Table then
 		for Index, Value in next, Table do
 			local Characters = string.split(string.lower(Index), "")
@@ -11,9 +11,9 @@ return function(Table)
 				rawset(Table, Character, rawget(Table, Character) or {Value})
 				Table = rawget(Table, Character)
 			end
-			
+
 			local Children = rawget(Table, 2)
-				
+
 			if Children then
 				rawset(Children, #Children + 1, Value)
 			else
@@ -21,17 +21,17 @@ return function(Table)
 			end
 		end
 	end
-	
+
 	return setmetatable(Search, {
 		__call = function(self, Index)
 			local Characters = string.split(string.lower(Index), "")
-			
+
 			if #Characters == 1 then
-				return rawget(rawget(Search, Characters[1]), 2)
+				return rawget(rawget(Search, string.lower(Index)), 2)
 			end
-			
+
 			local Value = Search
-			
+
 			for _, Character in next, Characters do
 				if rawget(Value, Character) then
 					Value = rawget(Value, Character)
@@ -39,18 +39,18 @@ return function(Table)
 					return nil
 				end
 			end
-			
+
 			return Value[2]
 		end,
 		__index = function(self, Index)
 			local Characters = string.split(string.lower(Index), "")
-			
+
 			if #Characters == 1 then
-				return rawget(rawget(Search, Characters[1]), 1)
+				return rawget(rawget(Search, string.lower(Index)), 1)
 			end
-			
+
 			local Value = Search
-			
+
 			for _, Character in next, Characters do
 				if rawget(Value, Character) then
 					Value = rawget(Value, Character)
@@ -58,7 +58,7 @@ return function(Table)
 					return nil
 				end
 			end
-			
+
 			return Value[1]
 		end,
 		__newindex = function(self, Index, Value)
@@ -68,9 +68,9 @@ return function(Table)
 			for _, Character in next, Characters do
 				rawset(Table, Character, rawget(Table, Character) or {Value})
 				Table = rawget(Table, Character)
-				
+
 				local Children = rawget(Table, 2)
-				
+
 				if Children then
 					rawset(Children, #Children + 1, Value)
 				else
